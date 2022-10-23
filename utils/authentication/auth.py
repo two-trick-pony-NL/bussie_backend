65cf1571c76783f4d1d74288048eda4129af10ec
Peter van Doorn <pvandoorn@bunq.com>
2022-10-23 10:39:35 +0200

    added authentication to endpoints

--- utils/authentication/auth.py
+++ utils/authentication/auth.py
@@ -5,14 +5,24 @@ from starlette.status import HTTP_403_FORBIDDEN
 
 api_key_header = APIKeyHeader(name="access_token", auto_error=False)
 
-#This should be replaced with a database of users + API keys but for now we'll just use this list of keys
+# This should be replaced with a hashed database
+# of users + API keys but for now we'll just use this list of keys
 valid_API_Keys = ["PETER", "MENDEL"]
+
+"""
+Here is an example of a call with API token. Make sure to add it to the HEADER. 
+curl -X 'GET' \
+  'http://localhost:8000/get_closest_stations/?latitude=0.1&longitude=1.0' \
+  -H 'accept: application/json' \
+  -H 'access_token: PETER'
+
+"""
 
 # This function can be called and checks API keys
 async def get_api_key(api_key_header: str = Security(api_key_header)):
     if api_key_header in valid_API_Keys:
-        return api_key_header   
+        return api_key_header
     else:
         raise HTTPException(
-            status_code=HTTP_403_FORBIDDEN, detail="Could not validate API KEY"
+            status_code=HTTP_403_FORBIDDEN, detail="No valid API KEY in request"
         )
