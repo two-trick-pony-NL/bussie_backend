bfead566b4bfc867d113b104a01a3a175e02d528
Peter van Doorn <pvandoorn@bunq.com>
2022-10-31 16:56:57 +0100

    Moved back to port 80

--- Infrastructure/gunicorn.sh
+++ Infrastructure/gunicorn.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-uvicorn main:app --host=0.0.0.0 --port 8080
+uvicorn main:app --host=0.0.0.0 --port 80
 
 #gunicorn main:app -w 4 --threads 2 -b 0.0.0.0:80
 #gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80
