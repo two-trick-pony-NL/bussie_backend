4f50b97cf6f5191ad452e56dc62a906c471d1f3c
Peter van Doorn <pvandoorn@bunq.com>
2022-11-01 18:50:35 +0100

    removed second container again

--- bussie_backend/data_collection/livestream.py
+++ bussie_backend/data_collection/livestream.py
@@ -1,7 +1,6 @@
 from gzip import GzipFile
 from io import BytesIO
 import zmq
-from time import sleep, time
 import xml.etree.ElementTree as ET
 
 
@@ -16,18 +15,18 @@ while True:
     multipart = subscriber.recv_multipart()
     address = multipart[0]
     try:
-        # The data is sent gzipped compressed as bytes. This unpacks this and stores it in contents variable
-        contents = GzipFile('','r',0,BytesIO(multipart[1])).read()
+        # The data is sent gzipped
+        # compressed as bytes
+        # This unpacks this and stores it in contents variable
+        contents = GzipFile('', 'r', 0, BytesIO(multipart[1])).read()
         root = ET.fromstring(contents)
-        
-        
         print("Updates Received:")
         # More comments
-        #Gets the timestamp
+        # Gets the timestamp
         print('time', root[3].text)
-        #Gets the operator of this transport
-        print('operator: ',root[4][0][0].text)
-        #Gets the line number
+        # Gets the operator of this transport
+        print('operator: ', root[4][0][0].text)
+        # Gets the line number
         print('line: ', root[4][0][1].text)
         print('X Coord: ', root[4][0][12].text)
         print('Y Coord: ', root[4][0][13].text)
