dec6c4e520b7d65b327dc9e6b634d32a39085a6c
Peter van Doorn <pvandoorn@bunq.com>
2022-11-02 15:24:48 +0100

    redeploy

--- utils/analytics/locations.py
+++ utils/analytics/locations.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
+import time
 
-df = pd.pandas.read_json('http://localhost/API/V1/get_vehicles', orient='index')
-
-df = pd.DataFrame(df,
-    columns=['linenumber','latitude', 'longitude'])
-
-print(df)
-st.map(df)
+with st.empty():
+    while True:
+        df = pd.pandas.read_json('http://localhost/API/V1/get_vehicles', orient='index')
+        df = pd.DataFrame(df,columns=['linenumber','latitude', 'longitude'])
+        st.map(df)
+        time.sleep(5)
