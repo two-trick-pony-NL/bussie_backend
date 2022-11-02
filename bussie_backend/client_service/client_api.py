66c89294f571b833046c1b5407c1eab280360d99
Peter van Doorn <pvandoorn@bunq.com>
2022-11-02 14:09:41 +0100

    added endpoint with vehicle lat/lon

--- bussie_backend/client_service/client_api.py
+++ bussie_backend/client_service/client_api.py
@@ -4,7 +4,10 @@ from bussie_backend.database import crud, models, schemas
 from sqlalchemy.orm import Session
 from ..database.database import SessionLocal, engine
 from ..calculations.calculate_closest_station import calculate_closest_station
-
+from ..data_collection.livestream import send_all_location, payload
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+import json
 
 from fastapi import APIRouter
 
@@ -37,3 +40,11 @@ async def get_closest_station(latitude: float, longitude: float | None = None):
 async def create_stop(stop: schemas.StopCreate,
                 db: Session = Depends(get_db)):
     return crud.create_stop(db=db, stop=stop)
+
+@router.get("/get_vehicles")
+def get_vehicle_location():
+    f = open('vehiclelocations.json')
+    data = json.load(f)
+    print(data)
+    f.close()
+    return data
